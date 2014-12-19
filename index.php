6da17479107d5a50d635802334d537c8a5897204
brandojohnson <brandon@rjohnsononline.com>
2014-12-18 23:03:58 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,138 @@
+/*
+Theme Name: Little Blind Spot
+Theme URI: http://www.brandojohnson.com
+Description: A theme built by Brandon Johnson 
+Author: Brandon Johnson
+Author URI: http://www.brandojohnson.com
+Version: 1.0
+Tags: responsive, white, bootstrap
+
+License: Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+License URI: http://creativecommons.org/licenses/by-sa/3.0/
+*/
+
+<%@ Language=VBScript%>
+
+<%
+Option Explicit
+%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xasp1/DTD/xhtm1-transitional.dtd">
+
+
+
+<html>
+<head>
+	<title>Window Treatments Hopkins MN | Little Blind Spot</title>
+	<meta name="DESCRIPTION" content="Little Blind Spot and Shutters N Shades in Hopkins, MN provides honest, quality customer service and expertise for residential and commercial window coverings.">
+	<meta name="KEYWORDS" content="little blind spot, blinds hopkins, residential shades hopkins, commercial shutters mn, residential shutters,shades hopkins, window grills, motorized blinds">
+	<link rel="STYLESHEET" type="text/css" href="misc/style.css">	
+	
+	<script type="text/javascript" src="js/RAA_FlashWriter.js"></script>		
+	<script type="text/vbscript" src="js/RAA_FlashWriter.vbs"></script>
+    <script type="text/javascript" src="js/swfobject.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>		
+	<script type="text/javascript">
+        var flashvars = {};
+        var params = {};
+        params.wmode = "opaque";
+        params.bgcolor = "#E9E3C1";
+        var attributes = {};
+        swfobject.embedSWF("flash/lbs_splashIMG_v1.swf", "splashFlash", "626", "346", "9.0.0", false, flashvars, params, attributes);
+    </script>
+
+
+<div class="maincontainer">
+	<!--GLOBAL PAGE TOP -->
+	<!--#include file = "includes/header.asp" -->
+
+	<div class="pagecontainer">
+		<!--PAGE CONTENT -->
+		<div class="lfloat" style="width:626px;margin:0px 46px 0px 31px;">
+			<div id="splashFlash">
+				<img src="/images/splash_noFlash.jpg" alt="blinds and window treatment" width="626" height="346" border="0">
+			</div>
+			<br>
+			
+			<div style="margin:25px 0px 10px 26px;float:left;width:600px;">
+				<div class="grayhead">
+					Providing Solutions of Beauty and Function Specific to Your Needs
+				</div>
+			
+				<div style="width:600px;margin:10px 0px 0px 0px;">
+					Located in <b>Hopkins, Minnesota</b>, Little Blind Spot has been providing window coverings for <a href="http://www.littleblindspot.com/residential/residential.asp" title="residential shutters mn">residential</a> 
+					and <a href="http://www.littleblindspot.com/commercial/commercial.asp" title="commercial window shades hopkins">commercial</a> markets since 1978. Family owned and operated, you can expect quality customer service and expertise 
+					at competitive prices, always!  
+					<br>
+					<a title="Learn more about little Blind Spot" href="/misc/about.asp">Learn more about us...</a>
+					<ul>
+						<li><a href="http://www.littleblindspot.com/misc/services.asp" title="Window Repairs and Installation">Repairs, Parts & Cleaning</a></li>
+						<li><a href="http://www.littleblindspot.com/residential/res-shutters.asp" title="Interior Wood Shutters">Interior Wood Shutters</a></li>
+						<li><a href="http://www.shuttersnshades.com/" title="Commercial Window Shutters">Exterior Rolling Shutters</a></li>
+						<li><a href="http://www.littleblindspot.com/commercial/commercial.asp" title="Window Motorization & Automation">Motorization & Automation</a></li>
+                        <li><a href="http://www.littleblindspot.com/misc/designers.asp" title="For Interior Designers">For Interior Designers</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	
+		<div class="lfloat" style="width:264px;margin:0px 0px 0px 0px;">
+			<img src="/images/home_pix1.jpg" width="234" height="86" alt="window treatments with blinds">
+			<div class="brownhead">
+				<a href="http://www.hdwfg.com/site.aspx?d=6983aea3-ae2d-4096-8b07-1af419424e5c&m1=HunterDouglas&m2=DealerLocator&d1="
+							onclick="openThis(this.href,1024,768);return false;">Hunter Douglas Gallery</a>
+			</div>
+			Discover the latest trends in Window Fashions online and at our Gallery Showroom in Hopkins.
+			<br>
+			<br>
+				
+			<img src="/images/home_pix2.jpg" width="234" height="77" alt="window treatments with blinds hopkins">
+			<div class="brownhead">
+				<a href="http://www.shuttersnshades.com/" onclick="openThis(this.href,1024,768);return false;">Shutters N Shades</a>
+			</div>
+			Our sister company provides exterior shade solutions, motorization options and more...
+			<br>
+			<br>
+				
+			<a href="/misc/gallery.asp"><img src="/images/residential_shutters.gif" width="234" height="81" alt="blinds and window treatment hopkins"></a>
+			<div class="brownhead">
+				<a href="/misc/gallery.asp">LBS Past Work Gallery</a>
+			</div>
+			See some interior and exterior shutters from past installations by Little Blind Spot.
+			<br>
+			<br>
+            
+            <div style="float: left; padding: 4px 8px 0px 0px;"><a href="/residential/residential.asp"><img src="/images/residential_blinds3.jpg" width="81" height="81" alt="residential window treatments" /></a></div>
+            
+            
+		  <div style="float: left; width:150px;" class="brownhead">
+			  <a href="/residential/residential.asp">Residential Window Specialists</a>
+			</div>	
+            Create the perfect, personalized scene with the help of our residential window covering experts.<br>
+            <br>
+
+		</div>
+
+
+		<div style="margin:475px 0 0 265px;;text-align:right;position:absolute;">
+			<a title="Click for the Business Review of Little Blind Spot, a Window Shades in Hopkins MN" 
+			href="http://www.bbb.org/minnesota/business-reviews/window-shades/little-blind-spot-in-hopkins-mn-96011495#sealclick">
+			<img alt="Little Blind Spot is a BBB Accredited Business. Click for the BBB Business Review of this Window Shades in Hopkins MN" 
+			style="border:0;padding:0;margin:0 0 20px 0;" src="http://seal-minnesota.bbb.org/seals/blue-seal-200-42-littleblindspot-96011495.png" /></a>				
+			
+			
+
+
+			<script type="text/javascript" src="http://www.angieslist.com/webbadge/insertwebbadge.js?bid=513c0d4bc726ef97ca2b91a8bc366c2a"></script><script type="text/javascript">if (BADGEBOX) BADGEBOX.PlaceBadge();</script><noscript><div id="ssanslnk"><a href="http://www.angieslist.com/companylist/us/mn/hopkins/little-blind-spot-reviews-141377.aspx" title="Angie's List Super Service Award winner">TWIN CITIES window treatments</a></div></noscript>
+
+		</div>
+	
+		<br clear="all">
+		<br>
+
+		<!--GLOBAL FOOTER -->
+		<!--#include file = "includes/footer.asp" -->
+	</div>
+</div>
+</body>
+</html>
+
